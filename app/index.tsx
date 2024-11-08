02e81f67ce31dbd31e9d64f1226aacebb2c12db4
Farouq Seriki <farouq@basaltx.io>
2024-11-08 07:30:08 +0100

    Project setup

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Canvas,useImage,Image } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
+
+const App = () => {
+const {width,height}=useWindowDimensions()
+const bg =useImage(require('../assets/sprites/background-day.png'))
+  const r = width * 0.33;
+  return (
+    <Canvas style={{ width, height }}>
+      <Image image={bg} width={width} height={height} fit={"cover"}/>
+    </Canvas>
+  );
+};
+ 
+export default App;
